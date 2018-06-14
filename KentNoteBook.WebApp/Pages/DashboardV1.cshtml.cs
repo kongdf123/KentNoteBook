9e9a19bf7b8d3da53acfdb268e84f4550bb61c40
Kent <kongdongfang@shinetechchina.com>
2018-06-14 17:20:45 +0800

    Add the responsive page

---
+++ KentNoteBook.WebApp/Pages/DashboardV1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KentNoteBook.WebApp.Pages
+{
+    public class DashboardV1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
