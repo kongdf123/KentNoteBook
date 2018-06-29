2405b6d3b9e3fe46ad3e6865041a353d29bd441b
Kent <kongdongfang@shinetechchina.com>
2018-06-29 15:16:57 +0800

    Add the bootstrap datepicker

---
+++ KentNoteBook.Infrastructure/Mvc/CustomResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KentNoteBook.Infrastructure.Mvc
+{
+	public class CustomResult<T> : ActionResult
+		where T : class
+	{
+		public CustomResult(int code, T data) {
+			this.Code = code;
+			this.Data = data;
+		}
+
+		/// <summary>
+		/// To standard the result type,  for instance , 1:success, 0:failure ...
+		/// </summary>
+		public int Code { get; set; }
+
+		public T Data { get; set; }
+	}
+
+	public class SuccessResult : CustomResult<string>
+	{
+		public SuccessResult() : base(1, "Successful") {
+		}
+	}
+}
--- KentNoteBook.WebApp/Pages/UserAccounts/Dialog/UserEdit.cshtml.cs
+++ KentNoteBook.WebApp/Pages/UserAccounts/Dialog/UserEdit.cshtml.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 using KentNoteBook.Data;
+using KentNoteBook.Infrastructure.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace KentNoteBook.WebApp.Pages.UserAccounts.Dialog
 {
 	public class UserEditModel : PageModel
 	{
 		readonly KentNoteBookDbContext _db;
+
 		public UserEditModel(KentNoteBookDbContext db) {
 			this._db = db;
 		}
+
+		[BindProperty]
+		public Guid? Id { get; set; }
 
 		[FromForm, BindProperty]
 		public UserModel Data { get; set; }
 
 		public class UserModel
 		{
-			public Guid Id { get; set; }
-
 			[Required]
 			public string Name { get; set; }
 
 			[StringLength(30)]
 			public string NickName { get; set; }
-
-			[StringLength(50), Required]
-			public string Password { get; set; }
 
 			[StringLength(100), Required]
 			public string Email { get; set; }
@@ -46,8 +49,42 @@ namespace KentNoteBook.WebApp.Pages.UserAccounts.Dialog
 			public Status Status { get; set; }
 		}
 
-		public void OnGet() {
+		public async Task<IActionResult> OnGetAsync() {
 
+			if ( !this.Id.HasValue ) {
+				this.Data = new UserModel();
+				this.Id = Guid.NewGuid();
+			}
+			else {
+				this.Data = await _db.Users
+					.AsNoTracking()
+					.Where(x => x.Id == this.Id)
+					.Select(x => new UserModel {
+						Name = x.Name,
+						NickName = x.NickName,
+						Email = x.Email,
+						Mobile = x.Mobile,
+						Avatar = x.Avatar,
+						Discription = x.Discription,
+						IsActive = x.IsActive,
+						Status = x.Status
+					})
+					.SingleOrDefaultAsync();
+
+				if ( this.Data == null ) {
+					return new BadRequestResult();
+				}
+			}
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync() {
+
+
+
+
+			return new SuccessResult();
 		}
 	}
 }
