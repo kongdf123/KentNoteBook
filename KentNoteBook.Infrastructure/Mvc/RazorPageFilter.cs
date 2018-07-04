37249a57c6adfbbfeba4b304754acaba84a52900
Kent <kongdf123@hotmail.com>
2018-07-04 08:56:31 +0800

    Add the PageFilter to intercept client request

---
+++ KentNoteBook.Infrastructure/Mvc/RazorPageFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace KentNoteBook.Infrastructure.Mvc
+{
+	public class RazorPageFilter : IPageFilter
+	{
+		readonly ILogger _logger;
+
+		public RazorPageFilter(ILogger logger) {
+			_logger = logger;
+		}
+
+		public void OnPageHandlerSelected(PageHandlerSelectedContext context) {
+			_logger.LogDebug("Global sync OnPageHandlerSelected called.");
+		}
+
+		public void OnPageHandlerExecuting(PageHandlerExecutingContext context) {
+			_logger.LogDebug("Global sync PageHandlerExecutingContext called.");
+		}
+
+		public void OnPageHandlerExecuted(PageHandlerExecutedContext context) {
+			_logger.LogDebug("Global sync OnPageHandlerExecuted called.");
+		}
+	}
+}
