1eb1ac27f91358a0aff120bb95e167325b6cd127
Kent <kongdongfang@shinetechchina.com>
2018-09-03 17:43:39 +0800

    Add a Vue project

---
+++ KentNoteBookSite.Vue/Pages/About.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KentNoteBookSite.Vue.Pages
+{
+	public class AboutModel : PageModel
+	{
+		public string Message { get; set; }
+
+		public void OnGet() {
+			Message = "Your application description page.";
+		}
+	}
+}
