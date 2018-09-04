431331dedafdae3482481e25eda1e23218329b7b
Kent <kongdongfang@shinetechchina.com>
2018-09-04 14:16:14 +0800

    Fix vulnerabilities by using npm command

--- KentNoteBookSite.Vue/Startup.cs
+++ KentNoteBookSite.Vue/Startup.cs
@@ -29,6 +29,11 @@ namespace KentNoteBookSite.Vue
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			// In production, the React files will be served from this directory
+			services.AddSpaStaticFiles(configuration => {
+				configuration.RootPath = "ClientApp/build";
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,10 +45,25 @@ namespace KentNoteBookSite.Vue
 				app.UseExceptionHandler("/Error");
 			}
 
-			app.UseStaticFiles();
 			app.UseCookiePolicy();
 
-			app.UseMvc();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
+
+			app.UseMvc(routes => {
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action=Index}/{id?}");
+			});
+
+			app.UseSpa(spa => {
+				spa.Options.SourcePath = "ClientApp";
+
+				if ( env.IsDevelopment() ) {
+					spa.UseProxyToSpaDevelopmentServer("http://localhost:8080");
+				}
+			});
 		}
 	}
 }
