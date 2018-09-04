082f33ed6473f808d421a2b44d2538a8369bd43d
Kent <kongdongfang@shinetechchina.com>
2018-09-04 17:21:18 +0800

    Add the AspNetCore source codes

---
+++ KentNoteBook.Infrastructure/AspNetCore/EventedStreamStringReaderCustom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.NodeServices.Util
+{
+	public class EventedStreamStringReaderCustom : IDisposable
+	{
+		private EventedStreamReaderCustom _eventedStreamReader;
+		private bool _isDisposed;
+		private StringBuilder _stringBuilder = new StringBuilder();
+
+		public EventedStreamStringReaderCustom(EventedStreamReaderCustom eventedStreamReader) {
+			_eventedStreamReader = eventedStreamReader
+				?? throw new ArgumentNullException(nameof(eventedStreamReader));
+			_eventedStreamReader.OnReceivedLine += OnReceivedLine;
+		}
+
+		public string ReadAsString() => _stringBuilder.ToString();
+
+		private void OnReceivedLine(string line) => _stringBuilder.AppendLine(line);
+
+		public void Dispose() {
+			if ( !_isDisposed ) {
+				_eventedStreamReader.OnReceivedLine -= OnReceivedLine;
+				_isDisposed = true;
+			}
+		}
+	}
+}
---
+++ KentNoteBook.Infrastructure/AspNetCore/TcpPortFinderCustom.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Microsoft.AspNetCore.SpaServices.Util
+{
+	public static class TcpPortFinderCustom
+	{
+		public static int FindAvailablePort() {
+			var listener = new TcpListener(IPAddress.Loopback, 0);
+			listener.Start();
+			try {
+				return ((IPEndPoint)listener.LocalEndpoint).Port;
+			}
+			finally {
+				listener.Stop();
+			}
+		}
+	}
+}
